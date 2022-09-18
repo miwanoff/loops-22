3c1a21fbfe011e1419532a3a755aae97822072b1
miwanoff <marina.iva.noffa@gmail.com>
2022-09-18 16:15:22 +0300

    Add dynamic

--- table.php
+++ table.php
@@ -1,6 +1,7 @@
 <?php
 print "<table style=\"border:solid;\">";
 for ($i = 0; $i < 10; $i++) {
+    //${"x".$i} = 10;
     print "<tr><td style=\"border:solid;\">dfsdfs</td><td style=\"border:solid;\">dfsdfs</td></tr>";
 }
 print "</table>";
