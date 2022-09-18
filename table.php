1d22f0734852994be6b74b44132a603e6f3d8ef7
miwanoff <marina.iva.noffa@gmail.com>
2022-09-18 16:07:30 +0300

    First commit

---
+++ table.php
@@ -0,0 +1,6 @@
+<?php
+print "<table style=\"border:solid;\">";
+for ($i = 0; $i < 10; $i++) {
+    print "<tr><td style=\"border:solid;\">dfsdfs</td><td style=\"border:solid;\">dfsdfs</td></tr>";
+}
+print "</table>";
